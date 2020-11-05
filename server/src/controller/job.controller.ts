9d9d0f79c094057616aa9887925f9a9872aff43a
Abid Hasan <abidhasan@gmail.com>
2020-11-05 08:55:24 -0800

    Add create job route

--- server/src/controller/job.controller.ts
+++ server/src/controller/job.controller.ts
@@ -3,11 +3,14 @@
  */
 import express, { RequestHandler } from 'express';
 import passport from 'passport';
+import { validate } from 'class-validator';
 import * as jobService from '../service/job.service';
 import * as authService from '../service/auth.service';
 import { Role } from '../entity/user.entity';
 import { IJWTPayload } from './auth.controller';
-import { INSUFFICIENT_PRIVILEGE } from '../common/responseErrors';
+import { INSUFFICIENT_PRIVILEGE, INVALID_JOB_SCHEMA } from '../common/responseErrors';
+import { Job } from '../entity/job.entity';
+
 /**
  * Router Definition
  */
@@ -21,13 +24,41 @@ const handleGetJobs: RequestHandler = async (req, res) => {
   const user = await authService.findUserByEmail((req.user as IJWTPayload).email);
 
   if (user?.role === Role.JOB_CREATOR) {
-    return res.status(200).send(await jobService.getJobsForUser(user.id));
+    return res.status(200).json(await jobService.getJobsForUser(user.id));
   }
 
-  return res.status(401).send({ error: INSUFFICIENT_PRIVILEGE })
+  return res.status(401).json(INSUFFICIENT_PRIVILEGE);
+};
+
+const handleCreateJob: RequestHandler = async (req, res) => {
+  // Create job if user is JOB_CREATOR role
+  const user = await authService.findUserByEmail((req.user as IJWTPayload).email);
+
+  if (user?.role === Role.JOB_CREATOR) {
+    const newJob = new Job();
+    newJob.name = req.body.name;
+    newJob.description = req.body.description;
+    newJob.journal = req.body.journal;
+    newJob.language = req.body.language;
+    newJob.pay = req.body.pay;
+    newJob.owner = user;
+
+    try {
+      if ((await validate(newJob)).length) {
+        throw new Error(INVALID_JOB_SCHEMA);
+      }
+
+      return res.status(200).json(await jobService.createJob(newJob));
+    } catch (error) {
+      return res.status(400).json(error.message);
+    }
+  } else {
+    return res.status(401).json(INSUFFICIENT_PRIVILEGE);
+  }
 };
 
 /**
  * Controller routes
  */
 jobController.get('/', passport.authenticate('jwt', { session: false }), handleGetJobs);
+jobController.post('/', passport.authenticate('jwt', { session: false }), handleCreateJob);
--- server/src/service/job.service.ts
+++ server/src/service/job.service.ts
@@ -3,6 +3,14 @@ import { Job } from '../entity/job.entity';
 
 export const getJobsForUser = async (ownerId: number): Promise<Job[]> => {
   const entityManager = getManager();
-
-  return await entityManager.find(Job, { where: { owner: ownerId, } }) || [];
+return await entityManager.find(Job, { where: { owner: ownerId, } }) || [];
 };
+
+export const createJob = async (newJob: Job) => {
+  const jobRepository = getRepository(Job);
+  try {
+    return await jobRepository.save(jobRepository.create(newJob));
+  } catch (error) {
+    throw new Error(error);
+  }
+}
